9ca87db5167279fdedee8be9b3edbf004588642a
abhishektumula <abhishektumula@gmail.com>
2025-05-15 21:01:15 +0530

    basic operations -> array

--- arrays/basic_operations.py
+++ arrays/basic_operations.py
@@ -1,1 +1,27 @@
-# todo : append, pop, insert, extend, hashing, use-set ...extend
+# basic operations of arrays are implemented in this file
+# it includes:
+# 1. append
+# 2. insert
+# 3. remove
+# 4. pop
+# 5. index
+# 6. count
+# 7. reverse
+# 8. sort
+# 9. extend
+# 10. copy
+# 11. clear
+# 12. slice
+# 13. concatenate
+# 14. repeat
+# 15. membership
+# 16. length
+# 17. min
+# 18. max
+# 19. sum
+# 20. average
+# 21. median
+# 22. mode
+# 23. standard deviation
+# 24. variance
+# 25. range
